ab391ff42acabda96b8755fa11996bca818bbd06
Joshua Packer <jpacker@redhat.com>
2021-06-16 13:41:34 -0400

    Replace machinepool with install_config in extra_vars
    Signed-off-by: Joshua Packer <jpacker@redhat.com>

--- build/before-make.sh
+++ build/before-make.sh
@@ -3,5 +3,5 @@
 # Copyright (c) 2020 Red Hat, Inc.
 ###############################################################################
 
-ln -sf ../../build/git-hooks/pre-commit .git/hooks/pre-commit
+#ln -sf ../../build/git-hooks/pre-commit .git/hooks/pre-commit
 ln -sf ../../build/git-hooks/commit-msg .git/hooks/commit-msg
