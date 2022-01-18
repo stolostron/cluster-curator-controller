b9c8fb48e071039a665f585538e8e19eced1d3bf
Yang Le <yangle@redhat.com>
2022-01-18 11:18:06 +0800

    org rename (#96)
    Signed-off-by: Yang Le <yangle@redhat.com>

--- cmd/manager/main.go
+++ cmd/manager/main.go
@@ -14,9 +14,9 @@ import (
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 
-	"github.com/open-cluster-management/cluster-curator-controller/controllers"
-	clusteropenclustermanagementiov1beta1 "github.com/open-cluster-management/cluster-curator-controller/pkg/api/v1beta1"
-	"github.com/open-cluster-management/cluster-curator-controller/pkg/jobs/utils"
+	"github.com/stolostron/cluster-curator-controller/controllers"
+	clusteropenclustermanagementiov1beta1 "github.com/stolostron/cluster-curator-controller/pkg/api/v1beta1"
+	"github.com/stolostron/cluster-curator-controller/pkg/jobs/utils"
 	// +kubebuilder:scaffold:imports
 )
 
