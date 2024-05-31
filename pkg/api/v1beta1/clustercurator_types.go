debfd320ffb5327c2074feeb18011d661385e6ab
fxiang1 <fxiang@redhat.com>
2024-05-31 11:23:12 -0400

    Update API description
    Signed-off-by: fxiang1 <fxiang@redhat.com>

--- pkg/api/v1beta1/clustercurator_types.go
+++ pkg/api/v1beta1/clustercurator_types.go
@@ -14,30 +14,32 @@ type ClusterCuratorSpec struct {
 	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
 
-	// This is the desired curation that will occur
+	// This is the desired curation that will occur. The supported options are 'install', 'upgrade', or 'destroy'.
 	// +kubebuilder:validation:Enum={install,scale,upgrade,destroy,delete-cluster-namespace}
 	DesiredCuration string `json:"desiredCuration,omitempty"`
 
 	// Points to the Cloud Provider or Ansible Provider secret, format: namespace/secretName
 	ProviderCredentialPath string `json:"providerCredentialPath,omitempty"`
 
-	// During an install curation run these Pre/Post hooks
+	// During an install curation run these pre/post hooks.
 	Install Hooks `json:"install,omitempty"`
 
-	// During an scale curation run these Pre/Post hooks
+	// During an scale curation run these pre/post hooks.
 	Scale Hooks `json:"scale,omitempty"`
 
-	// During an destroy curation run these **Pre hook ONLY**
+	// During an destroy curation run these hooks.
+	// Standalone clusters only support the prehook.
+	// Hosted clusters support both prehook/posthook.
 	Destroy Hooks `json:"destroy,omitempty"`
 
-	// During an upgrade curation run these
+	// During an upgrade curation run these hooks.
 	// +kubebuilder:validation:XValidation:rule="!(has(oldSelf.intermediateUpdate) && self.intermediateUpdate != oldSelf.intermediateUpdate)",message="The intermediateUpdate cannot be modified"
 	// +kubebuilder:validation:XValidation:rule="!(has(oldSelf.intermediateUpdate) && oldSelf.intermediateUpdate != '' && has(oldSelf.desiredUpdate) && self.desiredUpdate != oldSelf.desiredUpdate)",message="The desiredUpdate cannot be modified when intermediateUpdate exists"
 	// +kubebuilder:validation:XValidation:rule="!has(self.intermediateUpdate) || (has(self.desiredUpdate) && self.desiredUpdate != '')",message="The intermediateUpdate cannot be created if desiredUpdate is missing or empty"
 	// +kubebuilder:validation:XValidation:rule="!(has(self.intermediateUpdate) && !has(oldSelf.intermediateUpdate) && has(oldSelf.desiredUpdate) && oldSelf.desiredUpdate != '')",message="The intermediateUpdate cannot be added via update if desiredUpdate already exists"
 	Upgrade UpgradeHooks `json:"upgrade,omitempty"`
 
-	// Kubernetes job resource created for curation of a cluster
+	// Kubernetes job resource created for curation of a cluster.
 	CuratingJob string `json:"curatorJob,omitempty"`
 
 	// Inventory values are supplied for use with the pre/post jobs.
@@ -45,13 +47,13 @@ type ClusterCuratorSpec struct {
 }
 
 type Hook struct {
-	// Name of the Ansible Template to run in Tower as a job
+	// Name of the Ansible Template to run in the Ansible Tower as a job.
 	// +kubebuilder:validation:Required
 	Name string `json:"name"`
 
 	// Type of the Hook. For Job type, Ansible job template will be used.
 	// For Workflow type, Ansible workflow template will be used.
-	// If omitted, default to Job type.
+	// If omitted, defaults to Job type.
 	// +optional
 	// +kubebuilder:default=Job
 	Type HookType `json:"type,omitempty"`
@@ -62,35 +64,35 @@ type Hook struct {
 	ExtraVars *runtime.RawExtension `json:"extra_vars,omitempty"`
 
 	// A comma-separated list of tags to specify which sets
-	// of ansible tasks in a job should be run.
+	// of Ansible tasks in a job should be run.
 	// +optional
 	JobTags string `json:"job_tags,omitempty"`
 
 	// A comma-separated list of tags to specify which sets
-	// of ansible tasks in a job should not be run.
+	// of Ansible tasks in a job should not be run.
 	// +optional
 	SkipTags string `json:"skip_tags,omitempty"`
 }
 
 type Hooks struct {
 
-	// TowerAuthSecret is ansible secret used in template to run in tower
+	// TowerAuthSecret is an Ansible secret used in the template to provide authentication to an Ansbile tower.
 	// +kubebuilder:validation:Required
 	TowerAuthSecret string `json:"towerAuthSecret,omitempty"`
 
-	// Jobs to run before the cluster deployment
+	// Jobs to run before the cluster deployment.
 	Prehook []Hook `json:"prehook,omitempty"`
 
-	// Jobs to run after the cluster import
+	// Jobs to run after the cluster import.
 	Posthook []Hook `json:"posthook,omitempty"`
 
-	// When provided, this is a Job specification and overrides the default flow
+	// When provided, this is a Job specification and overrides the default flow.
 	// +kubebuilder:pruning:PreserveUnknownFields
 	OverrideJob *runtime.RawExtension `json:"overrideJob,omitempty"`
 
 	// JobMonitorTimeout defines the timeout for finding a job, the unit of this is minute.
-	// If job is found, the curator controller waits until the job becomes active.
-	// By default, it is 5 minutes
+	// If the job is found, the curator controller waits until the job becomes active.
+	// By default, it is 5 minutes.
 	// If its value is less than or equal to zero, the default value will be used.
 	// +optional
 	// +kubebuilder:default=5
@@ -99,7 +101,7 @@ type Hooks struct {
 
 type UpgradeHooks struct {
 
-	// TowerAuthSecret is ansible secret used in template to run in tower
+	// TowerAuthSecret is an Ansible secret used in the template to provide authentication to an Ansbile tower.
 	// +kubebuilder:validation:Required
 	TowerAuthSecret string `json:"towerAuthSecret,omitempty"`
 
@@ -118,7 +120,7 @@ type UpgradeHooks struct {
 	DesiredUpdate string `json:"desiredUpdate,omitempty"`
 
 	// Channel is an identifier for explicitly requesting that a non-default
-	// set of updates be applied to this cluster. The default channel will be
+	// set of updates be applied to this cluster. The default channel will
 	// contain stable updates that are appropriate for production clusters.
 	// +optional
 	Channel string `json:"channel,omitempty"`
@@ -128,40 +130,40 @@ type UpgradeHooks struct {
 	// +optional
 	Upstream string `json:"upstream,omitempty"`
 
-	// Jobs to run before the cluster upgrade
+	// Jobs to run before the cluster upgrade.
 	Prehook []Hook `json:"prehook,omitempty"`
 
-	// Jobs to run after the cluster upgrade
+	// Jobs to run after the cluster upgrade.
 	Posthook []Hook `json:"posthook,omitempty"`
 
-	// When provided, this is a Job specification and overrides the default flow
+	// When provided, this is a Job specification and overrides the default flow.
 	// +kubebuilder:pruning:PreserveUnknownFields
 	OverrideJob *runtime.RawExtension `json:"overrideJob,omitempty"`
 
 	// MonitorTimeout defines the monitor process timeout, the unit of this is minute.
-	// By default, it is 120 minutes
+	// By default, it is 120 minutes.
 	// If its value is less than or equal to zero, the default value will be used.
 	// +optional
 	// +kubebuilder:default=120
 	MonitorTimeout int `json:"monitorTimeout,omitempty"`
 }
 
-// ClusterCuratorStatus defines the observed state of ClusterCurator work
+// ClusterCuratorStatus defines the observed state of ClusterCurator work.
 type ClusterCuratorStatus struct {
 	// Track the conditions for each step in the desired curation that is being
-	// executed as a job
+	// executed as a job.
 	Conditions []metav1.Condition `json:"conditions,omitempty"`
 }
 
-// HookType indicates the type for the Hook. It can be 'Job' or 'Workflow'
+// HookType indicates the type for the hook. It can be 'Job' or 'Workflow'
 // +kubebuilder:validation:Enum=Job;Workflow
 type HookType string
 
 const (
-	// HookTypeJob, the Hook is an Ansible Job template
+	// HookTypeJob, the hook is an Ansible Job template
 	HookTypeJob HookType = "Job"
 
-	// HookTypeWorkflow, the Hook is an Ansible Workflow template
+	// HookTypeWorkflow, the hook is an Ansible Workflow template
 	HookTypeWorkflow HookType = "Workflow"
 )
 
@@ -169,13 +171,13 @@ const (
 
 // Operation contains information about a requested or running operation
 type Operation struct {
-	// Option for retrying a failed posthook job
+	// Option for retrying a failed posthook job. The supported options are 'installPosthook' or 'upgradePosthook'.
 	// +kubebuilder:validation:Enum={installPosthook,upgradePosthook}
 	RetryPosthook string `json:"retryPosthook,omitempty"`
 }
 
 // ClusterCurator is the custom resource for the clustercurators API.
-// This kind allows you to run Ansible prehook and posthook jobs before provisioning Hive or HyperShift
+// This kind allows you to run Ansible prehook and posthook jobs before provisioning a Hive or HyperShift cluster
 // and importing a cluster. Additionally, cluster upgrade and destroy operations are supported as well.
 type ClusterCurator struct {
 	metav1.TypeMeta   `json:",inline"`
@@ -188,7 +190,7 @@ type ClusterCurator struct {
 
 // +kubebuilder:object:root=true
 
-// ClusterCuratorList contains a list of ClusterCurator
+// ClusterCuratorList contains a list of ClusterCurator resources.
 type ClusterCuratorList struct {
 	metav1.TypeMeta `json:",inline"`
 	metav1.ListMeta `json:"metadata,omitempty"`
